bf4e451e38e47cb352e904cf11bf24d55fb39718
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-28 08:51:37 -0600

    First commit

---
+++ src/test/java/com/devstudios/store/app/gateway/store_gateway/StoreGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.app.gateway.store_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
